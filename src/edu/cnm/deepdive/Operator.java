6d3aec6eef578981e70346704132f3319e97d0ad
James Larivee <mr_15th_entertainer@yahoo.com>
2019-09-18 16:52:37 -0600

    Added Calculator that would run values from input.txt

--- src/edu/cnm/deepdive/Operator.java
+++ src/edu/cnm/deepdive/Operator.java
@@ -8,19 +8,33 @@ import java.util.Arrays;
  * output string.
  */
 public enum Operator {
-  /** Pops 2 values from stack, pushes sum of the 2 back onto stack. */
+  /**
+   * Pops 2 values from stack, pushes sum of the 2 back onto stack.
+   */
   ADD("+"),
-  /** Pops 2 values from stack, pushes difference of the 2 back onto stack. */
+  /**
+   * Pops 2 values from stack, pushes difference of the 2 back onto stack.
+   */
   SUBTRACT("-"),
-  /** Pops 2 values from stack, pushes product of the 2 back onto stack. */
+  /**
+   * Pops 2 values from stack, pushes product of the 2 back onto stack.
+   */
   MULTIPLY("*"),
-  /** Pops 2 values from stack, pushes quotient(real) of the 2 back onto stack. */
+  /**
+   * Pops 2 values from stack, pushes quotient(real) of the 2 back onto stack.
+   */
   DIVIDE("/"),
-  /** Pops 2 values from stack, pushes its square root back onto stack. */
+  /**
+   * Pops 2 values from stack, pushes its square root back onto stack.
+   */
   SQUARE_ROOT("sqrt"),
-  /** Pops 2 values from stack, pushes  of 1st raised to the second back onto stack. */
+  /**
+   * Pops 2 values from stack, pushes  of 1st raised to the second back onto stack.
+   */
   POWER("^"),
-  /** Pops 2 values from stack, pushes remainder after truncated division of the 2 back onto stack. */
+  /**
+   * Pops 2 values from stack, pushes remainder after truncated division of the 2 back onto stack.
+   */
   MODULO("%");
 
   private String token;
@@ -29,19 +43,14 @@ public enum Operator {
     this.token = token;
   }
 
-  /**
-   * Exercises the Operator enum by printing all of the enumerated values (as an array).
-   * @param args command line arguments (ignored).
-   */
-  public static void main(String[] args) {
-    System.out.println(Arrays.toString(Operator.values()));
-  }
-
   @Override
   public String toString() {
     return token;
   }
 
+  public static String tokenPattern() {
+    return "(?:^|\\s)(\\+|\\-|\\*|\\/|\\^|\\%|sqrt)(?:\\s|$)";
+  }
   // FIXME Add operate method w/ switch (later version will use @Override).
 
 
